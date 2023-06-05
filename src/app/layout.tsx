51afd1602addc6d4ce00cdb0969d9eaf83953c70
user.email like : "eaGraciliana@Gmail.com <user.email like : "eaGraciliana@Gmail.com>
2023-06-05 11:14:20 -0300

    CHAL-6 :wrench: chore: config font size project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Saira } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const saira = Saira({ 
+  weight:['300', '400', '500', '600'],
+subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={saira.className}>{children}</body>
     </html>
   )
 }
